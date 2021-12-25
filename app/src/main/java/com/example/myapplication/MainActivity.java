3cf3cb2c05235078adf3ba6981a09a9529002deb
Xiong Ben <xiongben1993@gmail.com>
2021-12-25 17:32:41 +0800

    [add] demo init

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
