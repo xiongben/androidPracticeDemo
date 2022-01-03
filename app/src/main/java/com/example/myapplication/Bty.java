0b7f0bd58d286f8ff36d22b16e0c83d67bee1562
Xiong Ben <xiongben1993@gmail.com>
2022-01-03 22:19:13 +0800

    [add] activity transfer data by class

--- app/src/main/java/com/example/myapplication/Bty.java
+++ app/src/main/java/com/example/myapplication/Bty.java
@@ -1,6 +1,8 @@
 package com.example.myapplication;
 
 import android.content.Intent;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,16 +10,30 @@ import com.example.myapplication.ui.User;
 
 public class Bty extends AppCompatActivity {
     private TextView tv;
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bty);
 
+        editText = findViewById(R.id.editText);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent();
+                i.putExtra("data",editText.getText().toString());
+                setResult(1,i);
+                finish();
+            }
+        });
+
         Intent i = getIntent(); //接收参数
         tv = (TextView) findViewById(R.id.tv);
 //        tv.setText(i.getStringExtra("data "));
-        User user = (User) i.getSerializableExtra("user");
+//        User user = (User) i.getSerializableExtra("user");
+        User user = i.getParcelableExtra("user");
         tv.setText(String.format("user info(name=%s,age=%d)",user.getName(),user.getAge()));
     }
 
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.myapplication;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+import androidx.annotation.Nullable;
 import com.example.myapplication.ui.User;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,20 +16,24 @@ import com.example.myapplication.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ActivityMainBinding binding;
-
+//    private ActivityMainBinding binding;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView1);
+
         findViewById(R.id.btnStartBAty).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(MainActivity.this, Bty.class);
 //                i.putExtra("data","hello,this is params data");
                 i.putExtra("user", new User("xiaoming",12));
-                startActivity(i);
+//                startActivity(i);
+                startActivityForResult(i,0);
             }
         });
 
@@ -44,6 +50,12 @@ public class MainActivity extends AppCompatActivity {
 //        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
 //        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 //        NavigationUI.setupWithNavController(binding.navView, navController);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable @org.jetbrains.annotations.Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        textView.setText(data.getStringExtra("data"));
     }
 
     @Override
