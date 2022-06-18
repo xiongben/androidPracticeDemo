2d6db8f0dcc2e08e31f38c6a4ba90d949644b8de
kakaxi <kakaxi@gmail.com>
2022-06-18 17:50:43 +0800

    [add] menu activity demo

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button1: Button = findViewById(R.id.button1)
+        button1.setOnClickListener{
+            Toast.makeText(this, "you click button1", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.add_item ->  Toast.makeText(this, "you click add", Toast.LENGTH_SHORT).show()
+            R.id.remove_item ->  Toast.makeText(this, "you click remove", Toast.LENGTH_SHORT).show()
+        }
+        return true
     }
 }
