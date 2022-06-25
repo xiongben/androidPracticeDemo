c98a5bf948c5efb44488519dc46261fb1e0d4412
kakaxi <kakaxi@gmail.com>
2022-06-25 20:16:10 +0800

    [add] service demo

--- app/src/main/java/com/example/myapplication/MyService.kt
+++ app/src/main/java/com/example/myapplication/MyService.kt
@@ -1,19 +1,54 @@
 package com.example.myapplication
 
-import android.app.Service
+import android.app.*
+import android.content.Context
 import android.content.Intent
+import android.graphics.BitmapFactory
+import android.os.Binder
+import android.os.Build
 import android.os.IBinder
 import android.util.Log
+import androidx.core.app.NotificationCompat
 
 class MyService : Service() {
 
+    private val mBinder = DownloadBinder()
+
+    class DownloadBinder : Binder() {
+
+        fun startDownload() {
+            Log.d("MyService", "startDownload executed")
+        }
+
+        fun getProgress(): Int {
+            Log.d("MyService", "getProgress executed")
+            return 0
+        }
+    }
+
     override fun onBind(intent: Intent): IBinder {
-        TODO("Return the communication channel to the service.")
+        return mBinder
     }
 
     override fun onCreate() {
         super.onCreate()
         Log.d("MyService", "onCreate executed")
+
+        val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel("my_service","前台service通知", NotificationManager.IMPORTANCE_DEFAULT)
+            manager.createNotificationChannel(channel)
+        }
+        val intent = Intent(this, MyService::class.java)
+        val pi = PendingIntent.getActivity(this, 0, intent, 0)
+        val notification = NotificationCompat.Builder(this, "my_service")
+            .setContentTitle("this is content title")
+            .setContentText("this is content text")
+            .setSmallIcon(R.drawable.small_icon)
+            .setLargeIcon(BitmapFactory.decodeResource(resources, R.drawable.large_icon))
+            .setContentIntent(pi)
+            .build()
+        startForeground(1, notification)
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
--- app/src/main/java/com/example/myapplication/ServiceDemo.kt
+++ app/src/main/java/com/example/myapplication/ServiceDemo.kt
@@ -1,10 +1,11 @@
 package com.example.myapplication
 
+import android.content.ComponentName
+import android.content.Context
 import android.content.Intent
-import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.os.Message
+import android.content.ServiceConnection
+import android.os.*
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -24,6 +25,22 @@ class ServiceDemo : AppCompatActivity() {
         }
     }
 
+    lateinit var downloadBinder: MyService.DownloadBinder
+
+    private val connection = object : ServiceConnection {
+
+        override fun onServiceConnected(name: ComponentName, service: IBinder) {
+            downloadBinder = service as MyService.DownloadBinder
+            downloadBinder.startDownload()
+            downloadBinder.getProgress()
+        }
+
+        override fun onServiceDisconnected(name: ComponentName) {
+            Log.d("MyService", "onServiceDisconnected")
+        }
+
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,6 +49,9 @@ class ServiceDemo : AppCompatActivity() {
         val changeTextBtn: Button = findViewById(R.id.changeTextBtn)
         val startServiceBtn: Button = findViewById(R.id.startServiceBtn)
         val stopServiceBtn: Button = findViewById(R.id.stopServiceBtn)
+        val bindServiceBtn: Button = findViewById(R.id.bindServiceBtn)
+        val unbindServiceBtn: Button = findViewById(R.id.unbindServiceBtn)
+
 
         changeTextBtn.setOnClickListener {
             thread {
@@ -49,6 +69,14 @@ class ServiceDemo : AppCompatActivity() {
         stopServiceBtn.setOnClickListener {
             val intent = Intent(this, MyService::class.java)
             stopService(intent)
+        }
+
+        bindServiceBtn.setOnClickListener {
+            val intent = Intent(this, MyService::class.java)
+            bindService(intent, connection, Context.BIND_AUTO_CREATE) // 绑定Service
+        }
+        unbindServiceBtn.setOnClickListener {
+            unbindService(connection) // 解绑Service
         }
     }
 }
