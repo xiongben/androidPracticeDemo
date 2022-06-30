14e464ecb93191635e21508a9d5bc9f434f356c5
kakaxi <kakaxi@gmail.com>
2022-06-30 22:50:16 +0800

    [add] LiveData demo

--- app/src/main/java/com/example/myapplication/JetpackDemoActivity.kt
+++ app/src/main/java/com/example/myapplication/JetpackDemoActivity.kt
@@ -5,6 +5,7 @@ import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.animation.Transformation
 import android.widget.Button
 import android.widget.TextView
 import androidx.core.content.edit
@@ -24,12 +25,35 @@ class MyObserver : LifecycleObserver {
     }
 }
 
+data class User(var firstName: String, var lastName: String, var age:Int)
+
+object Repository {
+    fun getUser(userId: String): LiveData<User> {
+        val liveData = MutableLiveData<User>()
+        liveData.value = User(userId, userId, 0)
+        return liveData
+    }
+}
+
 
 class MainViewModel(countReserved: Int) : ViewModel() {
+    private val userLiveData = MutableLiveData<User>()
+    private val userIdLiveData = MutableLiveData<String>()
     var counter = MutableLiveData<Int>()
+    val userName: LiveData<String> = Transformations.map(userLiveData) { user ->
+        "${user.firstName} ${user.lastName}"
+    }
+
+    val user: LiveData<User> = Transformations.switchMap(userIdLiveData){userId ->
+        Repository.getUser(userId)
+    }
 
     init {
         counter.value = countReserved
+    }
+
+    fun getUser(userId: String) {
+        userIdLiveData.value = userId
     }
 
     fun plusOne () {
@@ -62,6 +86,7 @@ class JetpackDemoActivity : AppCompatActivity() {
         val infoText: TextView = findViewById(R.id.infoText)
         val plusOneBtn: Button = findViewById(R.id.plusOneBtn)
         val clearBtn: Button = findViewById(R.id.clearBtn)
+        val getUserBtn: Button = findViewById(R.id.getUserBtn)
 
         sp = getPreferences(Context.MODE_PRIVATE)
         val countReserved = sp.getInt("count_reserved", 0)
@@ -72,8 +97,18 @@ class JetpackDemoActivity : AppCompatActivity() {
         plusOneBtn.setOnClickListener {
             viewModel.plusOne()
         }
+
+        getUserBtn.setOnClickListener {
+            val userId = (0..1000).random().toString()
+            viewModel.getUser(userId)
+        }
+
         viewModel.counter.observe(this, Observer { count ->
             infoText.text = count.toString()
+        })
+
+        viewModel.user.observe(this, Observer { user ->
+            infoText.text = user.firstName
         })
         refreshCounter()
     }
