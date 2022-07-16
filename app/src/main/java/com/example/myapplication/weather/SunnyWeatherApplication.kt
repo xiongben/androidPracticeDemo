f32306cf2c79919e8f4247d8865dd877ce07884b
kakaxi <kakaxi@gmail.com>
2022-07-16 22:20:29 +0800

    [add]

--- app/src/main/java/com/example/myapplication/weather/SunnyWeatherApplication.kt
+++ app/src/main/java/com/example/myapplication/weather/SunnyWeatherApplication.kt
@@ -1,4 +1,19 @@
 package com.example.myapplication.weather
 
-class SunnyWeatherApplication {
+import android.annotation.SuppressLint
+import android.app.Application
+import android.content.Context
+
+class SunnyWeatherApplication: Application() {
+    companion object {
+        const val TOKEN = "111111"
+        @SuppressLint("StaticFieldLeak")
+        lateinit var context: Context
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        context = applicationContext
+    }
+
 }
