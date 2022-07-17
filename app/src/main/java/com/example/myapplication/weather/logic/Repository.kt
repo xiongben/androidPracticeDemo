ee906ecb62e43611c3e8c3dc91520288ee7ddfb5
kakaxi <kakaxi@gmail.com>
2022-07-17 22:51:07 +0800

    [add]

--- app/src/main/java/com/example/myapplication/weather/logic/Repository.kt
+++ app/src/main/java/com/example/myapplication/weather/logic/Repository.kt
@@ -1,5 +1,25 @@
 package com.example.myapplication.weather.logic
 
+import androidx.lifecycle.liveData
+import com.example.myapplication.weather.logic.model.Place
+import com.example.myapplication.weather.logic.network.SunnyWeatherNetwork
+import kotlinx.coroutines.Dispatchers
+import java.lang.Exception
+import java.lang.RuntimeException
+
 object Repository {
-    
+    fun searchPlaces(query: String) = liveData(Dispatchers.IO) {
+        val result = try {
+            val placeResponse = SunnyWeatherNetwork.searchPlaces(query)
+            if (placeResponse.status == "ok") {
+                val places = placeResponse.places
+                Result.success(places)
+            } else {
+                Result.failure(RuntimeException("response status is ${placeResponse.status}"))
+            }
+        } catch (e: Exception) {
+            Result.failure<List<Place>>(e)
+        }
+        emit(result)
+    }
 }
