a213a9beb2f7c698c2ffc582d894dcb806cead02
kakaxi <kakaxi@gmail.com>
2022-07-21 23:30:24 +0800

    [add]

--- app/src/main/java/com/example/myapplication/weather/logic/Repository.kt
+++ app/src/main/java/com/example/myapplication/weather/logic/Repository.kt
@@ -2,8 +2,11 @@ package com.example.myapplication.weather.logic
 
 import androidx.lifecycle.liveData
 import com.example.myapplication.weather.logic.model.Place
+import com.example.myapplication.weather.logic.model.Weather
 import com.example.myapplication.weather.logic.network.SunnyWeatherNetwork
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
 import java.lang.Exception
 import java.lang.RuntimeException
 
@@ -19,6 +22,35 @@ object Repository {
             }
         } catch (e: Exception) {
             Result.failure<List<Place>>(e)
+        }
+        emit(result)
+    }
+
+    fun refreshWeather(lng: String, lat: String) = liveData(Dispatchers.IO) {
+        val result = try {
+            coroutineScope {
+               val deferredRealtime = async {
+                   SunnyWeatherNetwork.getRealtimeWeather(lng, lat)
+               }
+                val deferredDaily = async {
+                    SunnyWeatherNetwork.getDailyWeather(lng, lat)
+                }
+                val realtimeResponse = deferredRealtime.await()
+                val dailyResponse = deferredDaily.await()
+                if(realtimeResponse.status == "ok" && dailyResponse.status == "ok") {
+                    val weather = Weather(realtimeResponse.result.realtime, dailyResponse.result.daily)
+                    Result.success(weather)
+                } else {
+                    Result.failure(
+                        RuntimeException(
+                            "realtime response status is ${realtimeResponse.status}" +
+                                    "daily response status is ${dailyResponse.status}"
+                        )
+                    )
+                }
+            }
+        } catch (e: Exception) {
+            Result.failure<Weather>(e)
         }
         emit(result)
     }
